e9c109474774709406c914cd2db6d818049249a7
gyeong deok <littleduck1219@gmail.com>
2024-02-05 01:04:23 +0900

    [Design]sidebar#2

--- src/app/(landing)/_components/Heading.tsx
+++ src/app/(landing)/_components/Heading.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
-import React from "react";
+import Link from "next/link";
 
 export default function Heading() {
+    const { isLoading, isAuthenticated } = useConvexAuth();
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
@@ -12,11 +17,30 @@ export default function Heading() {
                 <span className="underline">Notion</span>
             </h1>
             <h3 className="text-base font-medium sm:text-xl md:text-2xl">
-                Notion is the connected workspace where better, faster work happens
+                Notion is the connected workspace where better, faster work happens.
             </h3>
             <Button>
-                Enter Notion
-                <ArrowRight className="ml-2 h-4 w-4" />
+                {isLoading && (
+                    <div className="flex w-full items-center justify-center">
+                        <Spinner size="lg" />
+                    </div>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <Button asChild>
+                        <Link href="/documents">
+                            Enter Notion
+                            <ArrowRight className="ml-2 h-4 w-4" />
+                        </Link>
+                    </Button>
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <SignInButton mode="modal">
+                        <Button>
+                            Get Notion Free
+                            <ArrowRight className="ml-2 h-4 w-4" />
+                        </Button>
+                    </SignInButton>
+                )}
             </Button>
         </div>
     );
--- src/app/(service)/_components/Navigation.tsx
+++ src/app/(service)/_components/Navigation.tsx
@@ -1,9 +1,39 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { ElementRef, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
 export default function Navigation() {
+    const isMobile = useMediaQuery("(max-width: 768px)");
+    const pathname = usePathname();
+    const isResizingRef = useRef(false);
+    const sidebarRef = useRef<ElementRef<"aside">>(null);
+    const navbarRef = useRef<ElementRef<"div">>(null);
+    const [isResetting, setIsResetting] = useState(false);
+    const [isCollapsed, setIsaCollapsed] = useState(isMobile);
+
     return (
         <>
-            <aside className="group relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary">
+            <aside
+                ref={sidebarRef}
+                className={cn(
+                    "group relative z-[99999] flex h-full w-60 flex-col overflow-y-auto bg-secondary",
+                    isResetting && "transition-all duration-300 ease-in-out",
+                    isMobile && "w-0",
+                )}
+            >
+                <div
+                    role="button"
+                    className={cn(
+                        "opacity absolute right-2 top-3 h-6 w-6 rounded-sm text-muted-foreground transition hover:bg-neutral-300 group-hover:opacity-100 dark:hover:bg-neutral-600",
+                        isMobile && "opacity-100",
+                    )}
+                >
+                    <ChevronsLeft className="h-6 w-6" />
+                </div>
                 <div>
                     <p>Action</p>
                 </div>
@@ -12,6 +42,20 @@ export default function Navigation() {
                 </div>
                 <div className="absolute right-0 top-0 h-full w-1 cursor-ew-resize bg-primary/10 opacity-0 transition group-hover:opacity-100" />
             </aside>
+            <div
+                ref={navbarRef}
+                className={cn(
+                    "w-[calc(100%-240px absolute left-60 top-0 z-[99999]",
+                    isResetting && "transition-all ease-in-out",
+                    isMobile && "left-0 w-full",
+                )}
+            >
+                <nav className="w-full bg-transparent px-3 py-2">
+                    {isCollapsed && (
+                        <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
+                    )}
+                </nav>
+            </div>
         </>
     );
 }
