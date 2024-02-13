4267b7cf8b3d33eb674bf5a67fd5935b8ba4ea20
gyeong deok <littleduck1219@gmail.com>
2024-02-13 23:32:19 +0900

    [Feat]Document nav

--- src/app/(service)/_components/Navbar.tsx
+++ src/app/(service)/_components/Navbar.tsx
@@ -1,10 +1,43 @@
 "use client";
 
+import { useQuery } from "convex/react";
+import { MenuIcon } from "lucide-react";
+import { useParams } from "next/navigation";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+
 interface NavbarProps {
     isCollapsed: boolean;
     onResetWidth: () => void;
 }
 
 export default function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
-    return <div></div>;
+    const params = useParams();
+
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId as Id<"documents">,
+    });
+
+    if (document === undefined) {
+        return <p>Loading...</p>;
+    }
+
+    if (document === null) {
+        return null;
+    }
+
+    return (
+        <>
+            <nav className="flex w-full items-center gap-x-4 bg-background px-3 py-2 dark:bg-[#1f1f1f]">
+                {isCollapsed && (
+                    <MenuIcon
+                        role="button"
+                        onClick={onResetWidth}
+                        className="h-6 w-6 text-muted-foreground"
+                    />
+                )}
+                <div className="flex w-full items-center justify-between">Navbar</div>
+            </nav>
+        </>
+    );
 }
